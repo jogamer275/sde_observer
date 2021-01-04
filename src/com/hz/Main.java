1bff5014dc8f3fe8d7c1eb79ae92432b87bf3243
Andries Nieuwenhuize <nieu0082@hz.nl>
2021-01-04 12:01:03 +0100

    Teacher added

--- src/com/hz/Main.java
+++ src/com/hz/Main.java
@@ -4,25 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // the gossiping 
 
-        Human olivia = new Human("Olivia");
-
-        Schoolyard schoolyard = new Schoolyard();
-
-        schoolyard.addStudents(olivia);
-
-
-        while(true) {
-
-            // Add a new student each time
-
-            Console.writeLine("Start a topic...");
-
-            String topic = Console.readLine();
-
-            schoolyard.dropATopic(topic);
-        }
 
     }
 }
